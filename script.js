86694d601de679f4144b00d1b6fc563475f9a0ec
״Dina” <dina10112003@gmail.com>
2026-06-23 21:55:49 +0300

    add blank cards and API logic

--- script.js
+++ script.js
@@ -20,3 +20,44 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(`You selected ${selectedTheme} theme`);
   });
 });
+
+
+const fetchImgByTheme = async (choosenTheme) => {
+  let images = [];
+  if(choosenTheme === 'dogs'){
+    try{
+      const response = await fetch('https://dog.ceo/api/breeds/image/random/6');
+      if(!response.ok) throw new Error('Failed at fetch dogs images');
+      const data = await response.json();
+      return data;
+    }
+    catch(error){
+      console.error('An error in dogs API',error.message);
+      return;
+    }
+  }
+  if(choosenTheme === 'Harry Potter'){
+    try{
+      const response = await fetch('https://hp-api.onrender.com/api/characters');
+      if(!response.ok) throw new Error('Failed at fetch Harry Potter character images');
+      const data = await response.json();
+      return data;
+    }
+    catch(error){
+      console.error('An error in Harry Potter API',error.message);
+      return;
+    }
+  }
+  if(choosenTheme === 'Country flags'){
+    try{
+      const response = await fetch('https://api.restcountries.com/countries/v5?limit=1&pretty=1');
+      if(!response.ok) throw new Error('Failed at fetch Country flags images');
+      const data = await response.json();
+      return data;
+    }
+    catch(error){
+      console.error('An error in Country flags API',error.message);
+      return;
+    }
+  }
+};
